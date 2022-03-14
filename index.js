bd7f58e5a2b5cdf0f9d566d4a7b3e33170229e2d
ryanpenber <rainpenber@gmail.com>
2022-03-15 03:41:48 +0800

    fix🐛:  增加排除、修复黄绿色提示排除
    ## 1.使用es中的every和some优化部分逻辑
    不好：
    ```js
            let count = 0;
            for (let char of yellowChars) {
                for (let el of charSet) {
                    if (el.indexOf(char) > -1) {
                        count += 1;
                        break;
                    }
                }
            }
            if (count < 1) { return false }
    ```
    改进：
    ```js
            // 对于每一个黄色拼音，至少要在一个字中找到它
            let checkYellowChar = yellowChars.every((char) => {
                charSet.some((el) => el.indexOf(char) > -1);
            });
            if (!checkYellowChar) { return false }
    ```
    ## 2.修复对三种颜色检测的逻辑
    - 绿色字说明内容正确、位置正确。所以绿色提示一定包含有位置信息，比对指定位置的内容即可
    - 黄色字说明内容正确、位置不明。所以应该保证至少出现一次（yellowArr.every => arr.some）
    - 灰色字说明根本没有这样的内容。所以应该保证所有内容中都不能出现它（! exclude.some => arr.some）
    ## 3.据此优化hint的数据结构

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 const fs = require("fs")
 const readline = require("readline")
 const pinyin = require("pinyin")
-const hint1 = [
-    {
-        char: "ing",
-        tone: null
-    },
-    {
-        char: "",
-        tone: 3
-    },
-    {
-        char: "xin",
-        tone: 1
-    },
-    {
-        char: "",
-        tone: 1
-    }
-]
 const hint = {
     green: [
         {
-            char: "xin",
-            tone: 1,
-            place: 3
+            char: "u",
+            tone: 4,
+            place: 1
+        },
+        {
+            char: "i",
+            tone: 4,
+            place: 2
         },
         {
             char: null,
+            tone: 2,
+            place: 3
+        },
+        {
+            char: "fei",
             tone: 1,
             place: 4
         }
     ],
     yellow: {
-        char: ["ing"],
-        tone: [3]
+        hanzi: [],
+        char: [],
+        tone: []
+    },
+    not: {
+        hanzi: ["非", "物", "是", "人"],
+        char: ["w","ren"],
+        tone: null
     }
 }
 const getPinYin = (hanzi) => {
@@ -60,36 +58,70 @@ const match = (idiom, hint) => {
         toneSet.push(el.tone);
     });
     const greenChars = hint["green"];
+    const yellowHanzi = hint["yellow"]["hanzi"];
     const yellowChars = hint["yellow"]["char"];
     const yellowTones = hint["yellow"]["tone"];
+    const exclusions = hint["not"];
+    // 检查排除项
+    if (
+        exclusions &&
+        Object.keys(exclusions).some((key) => exclusions[key] && exclusions[key].length > 0)
+    ) {
+        let hanziFlag = false, charFlag = false, toneFlag = false;
+        // 检测是否出现任何排除汉字
+        if (exclusions["hanzi"] && exclusions["hanzi"].length > 0) {
+            hanziFlag = exclusions["hanzi"].some((ex) => idiom.indexOf(ex) > -1)
+        }
+        if (hanziFlag) { return false }
+        // 检测是否出现任何排除拼音
+        if (exclusions["char"] && exclusions["char"].length > 0) {
+            charFlag = exclusions["char"].some((ex) => {
+                charSet.some((thisChar) => {thisChar.indexOf(ex) > -1})
+            })
+        }
+        if (charFlag) { return false }
+        // 检测是否出现任何排除音调
+        if (exclusions["tone"] && exclusions["tone"].length > 0) {
+            toneFlag = exclusions["tone"].some((ex) => toneSet.indexOf(ex) > -1)
+        }
+        if (toneFlag) { return false }
+    }
     // 检查绿色条件
     for (let hintChar of greenChars) {
         const thisChar = py[hintChar["place"]-1];
+        // 如果指定位置的字不对
+        if (hintChar["hanzi"]) {
+            if (idiom.split("")[hintChar["place"]-1 != hintChar["hanzi"]]) { return false }
+        }
+        // 如果指定位置的拼音不对
         if (hintChar["char"]) {
-            if (thisChar["char"] != hintChar["char"]) { return false }
-        } else {
+            if (thisChar["char"].indexOf(hintChar["char"]) < 0) { return false }
+        }
+        // 如果指定位置的音调不对
+        if (hintChar["tone"]) {
             if (thisChar["tone"] != hintChar["tone"]) { return false }
         }
     }
     // 检查黄色条件
+    // 检查是否有黄色汉字
+    if (yellowHanzi && yellowHanzi.length > 0) {
+        // 对于每一个黄色汉字，都要在成语中找到它
+        let checkYellowHanzi = yellowHanzi.every((hanzi) => idiom.indexOf(hanzi) > -1);
+        if (!checkYellowHanzi) { return false };
+    }
     // 检查是否有黄色拼音
     if (yellowChars && yellowChars.length > 0) {
-        let count = 0;
-        for (let char of yellowChars) {
-            for (let el of charSet) {
-                if (el.indexOf(char) > -1) {
-                    count += 1;
-                    break;
-                }
-            }
-        }
-        if (count < 1) { return false }
+        // 对于每一个黄色拼音，至少要在一个字中找到它
+        let checkYellowChar = yellowChars.every((char) => {
+            charSet.some((el) => el.indexOf(char) > -1);
+        });
+        if (!checkYellowChar) { return false }
     }
     // 检查是否有黄色音调
     if (yellowTones && yellowTones.length > 0) {
-        for (let tone of yellowTones) {
-            if (toneSet.indexOf(tone) < 0) { return false }
-        }
+        // 对于每一个音调，至少要出现一次
+        let checkYellowTone = yellowTones.every((tone) => toneSet.indexOf(tone) > -1);
+        if (!checkYellowTone) { return false }
     }
     return true
 }
@@ -97,7 +129,6 @@ const match = (idiom, hint) => {
 const r1 = readline.createInterface({
     input: fs.createReadStream("./idioms.txt")
 })
-// let lineNum = 1;
 r1.on("line", function(line) {
     if(match(line, hint)) {
         console.log(line);
